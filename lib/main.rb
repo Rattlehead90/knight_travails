3f1001cb65c49053e5d6d49fa7453f6848ff574d
Rattlehead90 <strogonov@seznam.cz>
2022-08-29 21:11:20 +0200

    Add #build_graph to Board class
    The method connects the chess tiles (or squares) in a fashion of a
    knight's move, the following graph may be used for a quick bfs later.

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,8 @@
+require_relative 'vertex.rb'
+
 # Board class
 class Board
-  attr_accessor :board
+  attr_accessor :board, :unraveling_vertex
 
   def initialize(width = 8, height = 8)
     @board = []
@@ -12,6 +14,20 @@ class Board
 
     @knight = [0, 0]
     @relative_moves = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+    @unraveling_vertex = build_graph
+  end
+
+  def build_graph(knight = @knight)
+    return Vertex.new(@board[0]) if @board.length == 1
+
+    knight_square_vertex = Vertex.new(@board.delete(knight)) if @board.include?(knight)
+    absolute_moves = absolute_moves(knight)
+    absolute_moves.each do |square|
+      square_vertex = Vertex.new(square)
+      square_vertex.add_edge(knight)
+      knight_square_vertex&.add_edge(square_vertex)
+    end
+    build_graph(@board.shift)
   end
 
   def absolute_moves(knight_position)
@@ -19,10 +35,8 @@ class Board
     @relative_moves.each do |move_direction|
       x = (move_direction[0] + knight_position[0])
       y = (move_direction[1] + knight_position[1])
-      if @board.include?([x, y])
-        absolute_moves << @board.delete([x, y])
-      end
+      absolute_moves << @board.delete([x, y]) if @board.include?([x, y])
     end
-    puts absolute_moves
+    absolute_moves
   end
 end
---
+++ lib/vertex.rb
@@ -0,0 +1,13 @@
+# Vertex helper class
+class Vertex
+  attr_accessor :value, :edges
+
+  def initialize(value)
+    @value = value
+    @edges = []
+  end
+
+  def add_edge(vertex)
+    @edges << vertex
+  end
+end
